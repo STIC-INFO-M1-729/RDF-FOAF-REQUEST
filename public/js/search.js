b2b81a31067a98405278c334729287e3b6cc116c
Caillet François <francois.caillet@gmail.com>
2014-12-12 10:46:57 +0100

    Merge branch 'featureSparql' of /Users/funflash/Dropbox/M1STICISC/S7/INFO729 Big Data/RDF-FOAF-REQUEST with conflicts.

--- public/js/search.js
+++ public/js/search.js
@@ -2,19 +2,12 @@ var searchModule = angular.module('searchModule', ['modalModule']);
 
 // service accessing the USER API of Pop
 decksModule.factory('Searchs', ['$http',function($http) {
-	var cors_headers = {
-				'Access-Control-Allow-Origin' : '*',
-				'Access-Control-Allow-Methods' : 'POST, GET, OPTIONS, PUT'
-			};
 	return {
 		get : function() {
-			return $http.get('/search');
+			return $http.get('/searchs');
 		},
 		getQuerySoft : function(searchData) {
 			return $http.get('/search/' + searchData);
-		},
-		postQuerycomplexe : function(searchData) {
-			return $http.post('/deck/' + searchData);
 		}
 	};
 }]);
@@ -23,18 +16,13 @@ decksModule.factory('Searchs', ['$http',function($http) {
 decksModule.controller('searchController', ['$scope','$http','Searchs',
 			 function($scope, $http, Searchs) {
 
+	$scope.dataReturn = {};
+
 	Searchs.get()
 		.success(function(data) {
+
 			$scope.filtreSearchs = data;
 	});
-
-	$scope.goRestSearch = function(search){
-		console.log('Listing Search Complexe');
-
-		console.log(search.person.value);
-		Searchs.postQuerycomplexe(search.person.value);
-	};
-
 
 	$scope.goSearch = function(){
 
@@ -42,12 +30,10 @@ decksModule.controller('searchController', ['$scope','$http','Searchs',
 		console.log('First Search like name');
 		Searchs.getQuerySoft($scope.formData.valSearch)
 			.success(function(data) {
-				//console.log(data);
-				//console.log(data[0].person.value);
-				//Modify
-				$scope.lsSearch = data;
-				//console.log($scope.lsSearch);
+
+				$scope.listeSearchs = data;
+
 			});
+
 		};
 }]);
-
