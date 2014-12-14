e9a82213093fba6b6829f130527be223ce72568a
Remi Mollard <remimollard38@gmail.com>
2014-12-14 12:07:35 +0100

    Integration Master Rémi

---
+++ app/Helper/searcher.js
@@ -0,0 +1,11 @@
+/**
+ * Created by zorg on 14/12/14.
+ */
+
+module.exports = function effectuerRecherche(recherche) {
+	var jsonProduit = [
+	{"slabel":recherche, "rlabel":"les", "olabel":"copains"},
+	{"slabel":"coucou", "rlabel":"les", "olabel":"amis"}
+	];
+	return jsonProduit;
+};
