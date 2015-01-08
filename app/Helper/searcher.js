05b9f4365121e81019fbbe70ed63490b0b815dfa
Céline de Roland <celinederoland@itpassion.info>
2015-01-08 14:28:29 +0100

    commentaires requetes céline

--- app/Helper/searcher.js
+++ app/Helper/searcher.js
@@ -27,27 +27,7 @@ module.exports = function construireRequete(recherche, options, termine) {
         optQuery += "UNION" + block("?r", recherche);
     }
     query = query + optQuery + "} LIMIT " + options.limitQuery;
-    //return query;
-
-    /*var requete = "SELECT DISTINCT * " +
-     "WHERE { {} UNION {" +
-     "?s ?r ?o ; rdfs:label \"Barack Obama\"@en, ?slabel . " +
-     "?o ?r1 ?o1 ; rdfs:label ?olabel . " +
-     "?o1 ?r2 ?o2 ; rdfs:label ?o1label . " +
-     "?o2 rdfs:label ?o2label .  " +
-     "?r rdfs:label ?rlabel .  " +
-     "?r1 rdfs:label ?r1label . " +
-     "?r2 rdfs:label ?r2label . " +
-     "FILTER(lang(?slabel) = 'en'). " +
-     "FILTER(lang(?rlabel) = 'en'). " +
-     "FILTER(lang(?olabel) = 'en'). " +
-     "FILTER(lang(?r1label) = 'en'). " +
-     "FILTER(lang(?o1label) = 'en'). " +
-     "FILTER(lang(?r2label) = 'en'). " +
-     "FILTER(lang(?o2label) = 'en'). " +
-     "} " +
-     "} LIMIT 3";*/
-
+  
     console.log(query);
 
     client.query(query).execute(function (error, results) {
@@ -180,11 +160,13 @@ function block(relation, recherche) {
     var askedLabel = "";
 
     return "{" +
-            // ----------
+            // Branche S -> O -> O1 -> O2
             "?s " + relation + " ?o. " +
             "?s ?r ?o. " +
             "?o ?r1 ?o1. " +
             "?o1 ?r2 ?o2. " +
+            // Recherche des labels pour représenter les objets par des mots
+            // plutôt que par des URI 
             "?s rdfs:label \"" + recherche + "\"@en." +
             "?s rdfs:label ?slabel. " +
             relation + " rdfs:label ?rlabel. " +
@@ -193,8 +175,10 @@ function block(relation, recherche) {
             "?o1 rdfs:label ?o1label. " +
             "?r2 rdfs:label ?r2label. " +
             "?o2 rdfs:label ?o2label. " +
-            "?o1 rdf:type foaf:Person. " +
+            // Dans la branche, les niveaux O et O2 sont des personnes
+            "?o rdf:type foaf:Person. " +
             "?o2 rdf:type foaf:Person. " +
+            // Les labels doivent être écrits en anglais
             "FILTER(lang(?slabel) = 'en'). " +
             "FILTER(lang(?rlabel) = 'en'). " +
             "FILTER(lang(?olabel) = 'en'). " +
@@ -205,6 +189,7 @@ function block(relation, recherche) {
             "}"
             + "UNION" +
             "{" +
+            // Branche dans l'autre sens : O2 -> O1 -> 0 -> S
             "?o2 " + relation + " ?o1." +
             "?o2 ?r2 ?o1." +
             "?o1 ?r1 ?o." +
